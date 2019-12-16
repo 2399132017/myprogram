168c4d99452004ab27fa0b289414cc66b2edec33
yuanzheng <yz16601383018@163.com>
2019-12-16 13:32:44 +0800

    第一次发布的内容

---
+++ Testpaper.Scanning/Testpaper.Scanning/WinScanning.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Testpaper.Scanning
+{
+    partial class WinScanning
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnScanning = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBoxMessage = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnScanning
+            // 
+            this.btnScanning.Location = new System.Drawing.Point(12, 12);
+            this.btnScanning.Name = "btnScanning";
+            this.btnScanning.Size = new System.Drawing.Size(75, 23);
+            this.btnScanning.TabIndex = 1;
+            this.btnScanning.Text = "开始扫描";
+            this.btnScanning.UseVisualStyleBackColor = true;
+            this.btnScanning.Click += new System.EventHandler(this.btnScanning_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 413);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(790, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatus
+            // 
+            this.toolStripStatus.Name = "toolStripStatus";
+            this.toolStripStatus.Size = new System.Drawing.Size(99, 17);
+            this.toolStripStatus.Text = "已经扫描5份试卷";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "导出数据";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBoxMessage
+            // 
+            this.richTextBoxMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxMessage.Location = new System.Drawing.Point(0, 41);
+            this.richTextBoxMessage.Name = "richTextBoxMessage";
+            this.richTextBoxMessage.Size = new System.Drawing.Size(790, 369);
+            this.richTextBoxMessage.TabIndex = 0;
+            this.richTextBoxMessage.Text = "扫描记录：";
+            // 
+            // WinScanning
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 435);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnScanning);
+            this.Controls.Add(this.richTextBoxMessage);
+            this.Name = "WinScanning";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "试卷扫描";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnScanning;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBoxMessage;
+    }
+}
+
---
+++ Testpaper.Scanning/Testpaper.Scanning/WinScanning.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testpaper.Scanning
+{
+    public partial class WinScanning : Form
+    {
+        public WinScanning()
+        {
+            InitializeComponent();
+        }
+        //开始扫描
+        private void btnScanning_Click(object sender, EventArgs e)
+        {
+
+        }
+        //导出数据
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
